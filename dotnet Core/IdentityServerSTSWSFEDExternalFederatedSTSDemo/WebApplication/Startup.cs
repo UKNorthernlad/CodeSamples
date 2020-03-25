d0ea4075e1a0a1b4267e0c44ac4088e0462d0a8c
Bobby Bob <real@outlook.com>
2020-03-25 23:27:41 +0000

    Added external federation example.

---
+++ dotnet Core/IdentityServerSTSWSFEDExternalFederatedSTSDemo/WebApplication/Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // https://docs.microsoft.com/en-us/aspnet/core/security/authentication/ws-federation?view=aspnetcore-3.1
+            // https://docs.microsoft.com/en-us/windows-server/identity/ad-fs/operations/manage-ssl-protocols-in-ad-fs
+            // https://c7solutions.com/2011/06/changing-adfs-20-endpoint-url-for-html
+            // https://docs.microsoft.com/en-us/windows-server/identity/ad-fs/operations/configure-ts-td-certs-ad-fs
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+            })
+                .AddWsFederation(options => {
+                    options.Wtrealm = Configuration["wsfed:realm"];
+                    options.MetadataAddress = Configuration["wsfed:metadata"];
+                }).AddCookie();
+
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                //options.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
---
+++ dotnet Core/IdentityServerSTSWSFEDExternalFederatedSTSDemo/is4inmem/CreateSigningKey.ps1
@@ -0,0 +1,19 @@
+﻿#Requires -RunAsAdministrator
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Remove old stuff
+Remove-Module -name "New-SelfSignedCertificateEx" -Force -ErrorAction SilentlyContinue -Confirm:$false
+Remove-Item "$HOME\Downloads\New-SelfSignedCertificateEx.zip" -Force -ErrorAction SilentlyContinue -Confirm:$false
+Remove-Item "$HOME\Downloads\New-SelfSignedCertificateEx" -Recurse -Force -ErrorAction SilentlyContinue -Confirm:$false
+Get-ChildItem "cert:\LocalMachine\My" | Where-Object {$_.Subject -eq "CN=sign"} | remove-item
+
+# Add new / overwrite existing
+Invoke-WebRequest -Uri "https://gallery.technet.microsoft.com/scriptcenter/Self-signed-certificate-5920a7c6/file/101251/2/New-SelfSignedCertificateEx.zip" -OutFile "$HOME\Downloads\New-SelfSignedCertificateEx.zip"
+
+Expand-Archive -Path "$HOME\Downloads\New-SelfSignedCertificateEx.zip" -DestinationPath "$HOME\Downloads\New-SelfSignedCertificateEx"
+
+Import-Module -FullyQualifiedName "$HOME\Downloads\New-SelfSignedCertificateEx\New-SelfSignedCertificateEx.ps1"
+
+New-SelfsignedCertificateEx -Subject "CN=sign" -SubjectAlternativeName "CN=localhost" -EKU "Server Authentication", "Client authentication" -KeyUsage "KeyEncipherment, DigitalSignature" -AllowSMIME -Path "$HOME\Downloads\sign.pfx" -Password (ConvertTo-SecureString "password" -AsPlainText -Force) -Exportable 
+
+$newcert = Import-PfxCertificate -FilePath "$HOME\Downloads\sign.pfx" -CertStoreLocation "cert:\localmachine\my" -Password (ConvertTo-SecureString "password" -AsPlainText -Force)
