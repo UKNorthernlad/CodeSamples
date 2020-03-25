1ca48d10db6fd04a49a5d3a58610fcd6605e974c
Bobby Bob <real@outlook.com>
2020-03-25 18:46:32 +0000

    Updated key creation script

--- dotnet Core/IdentityServerSTSWSFEDDemo/is4inmem/CreateSigningKey.ps1
+++ dotnet Core/IdentityServerSTSWSFEDDemo/is4inmem/CreateSigningKey.ps1
@@ -1,5 +1,14 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+# Remove old stuff
+Remove-Module -name "New-SelfSignedCertificateEx" -Force -ErrorAction SilentlyContinue -Confirm:$false
+Remove-Item "$HOME\Downloads\New-SelfSignedCertificateEx.zip" -Force -ErrorAction SilentlyContinue -Confirm:$false
+Remove-Item "$HOME\Downloads\New-SelfSignedCertificateEx" -Recurse -Force -ErrorAction SilentlyContinue -Confirm:$false
+Get-ChildItem "cert:\LocalMachine\My" | Where-Object {$_.Subject -eq "CN=sign"} | remove-item
+
+
+
+# Add new / overwrite existing
 Invoke-WebRequest -Uri "https://gallery.technet.microsoft.com/scriptcenter/Self-signed-certificate-5920a7c6/file/101251/2/New-SelfSignedCertificateEx.zip" -OutFile "$HOME\Downloads\New-SelfSignedCertificateEx.zip"
 
 Expand-Archive -Path "$HOME\Downloads\New-SelfSignedCertificateEx.zip" -DestinationPath "$HOME\Downloads\New-SelfSignedCertificateEx"
